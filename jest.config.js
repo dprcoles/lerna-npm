49af4e5a092e19cacc7bf4bc53a840d9eacefa70
Daniel Coles <me@danielcoles.co.uk>
2021-04-13 22:37:38 +0100

    refactor(config): updated repo config

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   snapshotSerializers: ['enzyme-to-json/serializer'],
-  collectCoverageFrom: ['src/**/*.js', '!src/index.js'],
+  collectCoverageFrom: ['src/**/*.js', 'src/**/*.jsx'],
+  moduleDirectories: ['node_modules', 'src'],
+  setupFiles: ['./enzyme.config.js'],
   coverageReporters: ['text'],
-  setupFilesAfterEnv: ['./enzyme.config.js'],
+  resetModules: true,
 }
